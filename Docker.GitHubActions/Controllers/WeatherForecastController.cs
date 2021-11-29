258d41b97dbd9910c8600bbf526a886a4fd8a523
Kedarnadhsharma <50028950+Kedarnadhsharma@users.noreply.github.com>
2021-11-29 16:56:14 +0530

    Update WeatherForecastController.cs

--- Docker.GitHubActions/Controllers/WeatherForecastController.cs
+++ Docker.GitHubActions/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Docker.GitHubActions.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Cold", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
